89b876b50bd92a59a926bbd08c0072eff95c5b91
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-11 11:55:42 -0500

    Data Layer(Capa de fuente de datos)

--- app/src/main/java/com/example/coupons/mainModule/model/RoomDatabase.kt
+++ app/src/main/java/com/example/coupons/mainModule/model/RoomDatabase.kt
@@ -1,4 +1,24 @@
 package com.example.coupons.mainModule.model
 
+import android.database.sqlite.SQLiteConstraintException
+import com.example.coupons.CouponsApplication
+import com.example.coupons.common.dataAccess.CouponDao
+import com.example.coupons.common.entities.CouponEntity
+import com.example.coupons.common.utils.Constants
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class RoomDatabase {
+    private val dao: CouponDao by lazy { CouponsApplication.dataBase.couponDao() }
+
+    suspend fun consultCouponByCode(code: String) = dao.consultCouponBy(code)
+
+    suspend fun saveCoupon(couponEntity: CouponEntity) = withContext(Dispatchers.IO) {
+        try {
+            dao.addCoupon(couponEntity)
+        }catch (exp: Exception){
+            (exp as? SQLiteConstraintException)?.let { throw Exception(Constants.ERROR_EXIST) }
+            throw Exception(exp.message ?: Constants.ERROR_UNKNOW)
+        }
+    }
 }
