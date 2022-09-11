71c4a3f56f332d8373fcdcfba981d93242107299
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-11 13:35:23 -0500

    Data Binding: Diseño(XML)

--- app/src/main/java/com/example/coupons/mainModule/view/MainActivity.kt
+++ app/src/main/java/com/example/coupons/mainModule/view/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupObservers() {
-        mainViewModel.getResult().observe(this){ coupon ->
+        /*mainViewModel.getResult().observe(this){ coupon ->
             if(coupon == null){
                 binding.tilDescription.hint = getString(R.string.main_hint_description)
                 binding.tilDescription.isEnabled = true
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tilDescription.isEnabled = false
                 binding.btnCreate.visibility = if(coupon.isActive) View.GONE else View.VISIBLE
             }
-        }
+        }*/
         mainViewModel.getSnackbarMsg().observe(this) { msg ->
             Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
         }
@@ -56,14 +56,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupButtons() {
         binding.btnConsult.setOnClickListener {
-            mainViewModel.consultCouponByCode(binding.etCoupon.text.toString())
+            //mainViewModel.consultCouponByCode(binding.etCoupon.text.toString())
             hideKeyboard(this, binding.root)
         }
         binding.btnCreate.setOnClickListener {
             val coupon = CouponEntity(
                 code = binding.etCoupon.text.toString()
                 , description = binding.etDescription.text.toString())
-            mainViewModel.saveCoupon(coupon)
+            //mainViewModel.saveCoupon(coupon)
             hideKeyboard(this, binding.root)
         }
     }
