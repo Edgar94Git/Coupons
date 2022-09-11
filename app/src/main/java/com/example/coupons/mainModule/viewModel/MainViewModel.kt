1f56d6d88afa4edc660be81fa6511ebecf9aa437
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-11 13:21:39 -0500

    Data Binding: Sate Holder

--- app/src/main/java/com/example/coupons/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/coupons/mainModule/viewModel/MainViewModel.kt
@@ -13,27 +13,35 @@ class MainViewModel: ViewModel() {
 
     private val repository = MainRepository()
 
-    private val result = MutableLiveData<CouponEntity>()
-    fun getResult() = result
+    private val coupon = MutableLiveData<CouponEntity>()
+
+    private val hideKeyboard = MutableLiveData<Boolean>()
+    fun isHideKeynoard() = hideKeyboard
 
     private val snackbarMsg = MutableLiveData<Int>()
     fun getSnackbarMsg() = snackbarMsg
 
-    fun consultCouponByCode(code: String){
-        viewModelScope.launch {
-            result.value = repository.consultCouponByCode(code)
+    fun consultCouponByCode(){
+        coupon.value?.code?.let { code ->
+            viewModelScope.launch {
+                hideKeyboard.value = true
+                coupon.value = repository.consultCouponByCode(code)
+            }
         }
     }
 
-    fun saveCoupon(couponEntity: CouponEntity){
-        viewModelScope.launch {
-            try {
-                repository.saveCoupon(couponEntity)
-                consultCouponByCode(couponEntity.code)
-                snackbarMsg.value = R.string.main_save_success
-            }
-            catch (e: Exception){
-                snackbarMsg.value = getMsgErrorByCode(e.message)
+    fun saveCoupon(){
+        coupon.value?.let { couponEntity ->
+            viewModelScope.launch {
+                hideKeyboard.value = true
+                try {
+                    repository.saveCoupon(couponEntity)
+                    consultCouponByCode()
+                    snackbarMsg.value = R.string.main_save_success
+                }
+                catch (e: Exception){
+                    snackbarMsg.value = getMsgErrorByCode(e.message)
+                }
             }
         }
     }
