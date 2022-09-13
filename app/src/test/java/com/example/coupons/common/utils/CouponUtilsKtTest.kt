e4b651f2ecf7a51431cc5bb938236d16f8a04d38
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-13 08:23:12 -0500

    Test para comparar valores(assetEquals)

--- app/src/test/java/com/example/coupons/common/utils/CouponUtilsKtTest.kt
+++ app/src/test/java/com/example/coupons/common/utils/CouponUtilsKtTest.kt
@@ -1,5 +1,6 @@
 package com.example.coupons.common.utils
 
+import com.example.coupons.R
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -26,5 +27,13 @@ class CouponUtilsKtTest{
     fun validateTextCodeMaxLengthTest(){
         val code = "Bienvenidos al curso"
         assertFalse(validateTextCode(code))
+    }
+
+    @Test
+    fun getMsgErrorByCodeNullTest(){
+        val errorCode = null
+        val expectedValue = R.string.error_unknow
+        val result = getMsgErrorByCode(errorCode)
+        assertEquals(expectedValue, result)
     }
 }
